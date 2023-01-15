5231f98a45757fe3d3cbfea34ca63e9647d8e315
Dominique Chin-Jones <domecj13@gmail.com>
2023-01-15 10:37:08 -0500

    render root with gql data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,47 @@
+import { useQuery, gql } from "@apollo/client";
+
+interface Game {
+  id: number
+  name: string
+  publisher: string
+  releaseDate: string
+  genre: { categories: string[] }
+}
 
 export default function App() {
+
+  const GAMES = gql`
+  query GetGames {
+    games {
+      id
+      name
+      publisher
+      releaseDate
+      genre {
+        categories
+      }
+    }
+  }
+  `;
+
+  const { loading, error, data } = useQuery(GAMES);
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
   return (
-      <h1>Hello, world!</h1>
+    <>
+      { data?.games.map((game: Game) => (
+          <div>
+          <h2>Game: {game.name}</h2>
+          <p>Publisher: {game.publisher}</p>
+          <p>Release Date: {game.releaseDate}</p>
+          <p>Genre: {game.genre.categories.map((name, index, parent) => (
+               index === parent.length-1 ? <span>{name}</span> : <span>{name}, </span> 
+            ))} </p>
+          </div>
+      ))}
+    </>
   );
 }
 
