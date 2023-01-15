46fa6dad0887a18f8ae3a967db079f8916bfb51d
Dominique Chin-Jones <domecj13@gmail.com>
2023-01-14 20:28:59 -0500

    config apollo server and seed mock data

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs, resolvers } from "./graphql/schema";
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+startStandaloneServer(server, {
+    listen: { port: 4000 },
+}).then(({ url }) => console.log(`Apollo Server ready at: ${url}`));
+
+// const { url } = await startStandaloneServer(server, {
+//     listen: { port: 4000 },
+// });
+// 
+// console.log(`Apollo Server ready at ${url}`);
