3218585419dffefe5447386d401e4f73f3dfead1
Dominique Chin-Jones <domecj13@gmail.com>
2023-01-15 09:55:25 -0500

    initialize react-router and apollo-client

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+import {
+    ApolloClient, 
+    InMemoryCache, 
+    ApolloProvider
+} from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "http://localhost:4000",
+    cache: new InMemoryCache(),
+});
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <ApolloProvider client={client}>
+          <RouterProvider router={router} />
+      </ApolloProvider>
   </React.StrictMode>,
 )
