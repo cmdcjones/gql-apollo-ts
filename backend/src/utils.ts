6d8789a204e110544967d83a738e38bcbeee85d4
Dominique Chin-Jones <domecj13@gmail.com>
2023-01-16 11:45:05 -0500

    create utils for data conversion

---
+++ backend/src/utils.ts
@@ -0,0 +1,7 @@
+// Helper functions to convert non-human readable data into human readable data
+// Useful for clean code and abstraction (more declarative)
+
+// Function for converting release data in epoch seconds to human readable date
+export function convertEpochSeconds(epochSeconds: number): void {
+}
+
