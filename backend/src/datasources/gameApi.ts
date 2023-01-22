0a7b07b89962ba22ad4d394fb0d4713d60f746ef
Dominique Chin-Jones <domecj13@gmail.com>
2023-01-22 08:46:57 -0500

    create TODOs

--- backend/src/datasources/gameApi.ts
+++ backend/src/datasources/gameApi.ts
@@ -2,6 +2,7 @@ import { AugmentedRequest, RESTDataSource } from "@apollo/datasource-rest";
 import * as dotenv from "dotenv";
 dotenv.config()
 
+// TODO: Create genre LUT for genre ID to genre name conversion
 export class GameAPI extends RESTDataSource {
     override baseURL = "https://api.igdb.com/v4/";
     override willSendRequest(_, requestOpts: AugmentedRequest) {
--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,9 +1,9 @@
 // Helper functions to convert non-human readable data into human readable data
 // Useful for clean code and abstraction (more declarative)
 
-// Function for converting release data in epoch seconds to human readable date
 const MILLISECONDS = 1000;
 
+// Function for converting release data in epoch seconds to human readable date
 export function convertEpochSeconds(epochSeconds: number): string {
     return new Date(epochSeconds * MILLISECONDS).toDateString();
 }
